27b8e272c62f47ac96f673756e173d06d33001f4
Jakub Vyvazil <jakub@vyvazil.cz>
2019-02-02 10:39:20 +0100

    implemented email value object

---
+++ src/Nofutur3/ValueObject/Email.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nofutur3\ValueObject;
+
+class Email extends Base
+{
+    /** @var string */
+    private $value;
+
+    public function __construct($value)
+    {
+        if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+            throw new \InvalidArgumentException('Email `'.$value.'` is not in valid format');
+        }
+
+        $this->value = $value;
+    }
+
+    public function __toString(): string
+    {
+        return $this->value;
+    }
+}
---
+++ tests/unit/EmailCest.php
@@ -0,0 +1,38 @@
+<?php
+
+class EmailCest
+{
+    public function _before(UnitTester $I)
+    {
+    }
+
+    /**
+     * @dataProvider emailProvider
+     */
+    public function testEmailValidation(UnitTester $I, \Codeception\Example $example)
+    {
+        if (!$example['result']) {
+            $I->expectThrowable(\InvalidArgumentException::class, function () use ($example) {new \Nofutur3\ValueObject\Email($example['email']); });
+        } else {
+            $I->assertSame($example['email'], (string) new \Nofutur3\ValueObject\Email($example['email']));
+        }
+    }
+
+    protected function emailProvider()
+    {
+        return [
+            ['email' => 'user@domain.com', 'result' => true],
+            ['email' => 'first.user@domain.com', 'result' => true],
+            ['email' => 'hello+user@domain.com', 'result' => true],
+            ['email' => 'user@domain.co.uk', 'result' => true],
+            ['email' => 'user@domain.cz', 'result' => true],
+            ['email' => 'user@subdomain.domain.com', 'result' => true],
+            // two below are limitations of filter, might be solved
+            // ['email' => 'user@10.10.10.1', 'result' => true],
+            // ['email' => 'user@domain.commonvalueobject', 'result' => false],
+            ['email' => 'user@hello', 'result' => false],
+            ['email' => 'user', 'result' => false],
+            ['email' => 'user.com', 'result' => false],
+        ];
+    }
+}
